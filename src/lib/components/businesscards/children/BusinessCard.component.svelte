a88621f2e1ea00bcdc6e5f4132000d6ce7a8a8aa
Temct <150273475+Defferentior@users.noreply.github.com>
2024-01-07 18:16:26 -0800

    Testing

--- src/lib/components/businesscards/children/BusinessCard.component.svelte
+++ src/lib/components/businesscards/children/BusinessCard.component.svelte
@@ -46,3 +46,234 @@
     <img src={businesscard.image} alt={businesscard.name} class={["businesscard-image","imgshow"].join(' ')} />
 </div>
 
+<style>
+
+.businesscard {
+  display: grid; /* Enable Grid */
+  flex-grow: 1;
+  box-sizing: border-box;
+  grid-template-areas:"image header header header"
+                      "image tags tags tags"
+                      "image links links links";
+  grid-template-columns: min(20%,200px) 1fr auto;
+  grid-template-rows: auto auto;
+  margin: 1em;
+  padding: .5rem;
+  min-height: 150px;
+  min-height: 200px;
+  max-height: 250px;
+  max-width: 800px;
+  position: relative;
+  text-align: left;
+  border-radius: 1em;
+}
+
+.businesscard-header {
+  display: flex;
+  flex-direction: column;
+  font-size: 1.2em;
+  font-weight: 600;
+  grid-area: header;
+  line-height: 1.4em;
+  min-width: 0;
+  position: relative
+}
+
+.businesscard-link {
+  border-radius: .25rem;
+  display: inline-block;
+  font-size: .625rem;
+  font-weight: 700;
+  line-height: 1.5em;
+  margin-bottom: auto;
+  margin-top: auto;
+  padding: 0 .375rem;
+}
+
+.businesscard-head-link {
+  border-radius: .25rem;
+}
+
+.businesscard-links {
+  display: flex;
+  grid-area: links;
+  
+}
+
+.businesscard-image {
+  transition: transform 0.3s ease-in-out, opacity 0.3s ease-in-out;
+  width: 85%; /* Adjust width as needed */
+  height: auto;
+  grid-area: image;
+  align-self: center;
+  margin-right: 1em; /* Space between image and links */
+  border-radius: 1em; /* Optional: Rounds the corners of the image */
+  background: var(--image-background); /* Use a CSS variable for the background */
+  padding: .4em; /* Adjust as needed, creates space around the image */
+  box-shadow: 0 0 8px rgba(0, 0, 0, 0.2); /* Optional: Adds shadow for depth */
+  object-fit: cover; /* Ensures the image covers the padding area */
+}
+
+.businesscard-image:hover {
+  transform: scale(1.05); /* Slightly enlarges the image */
+  opacity: 0.9; /* Slightly reduces opacity */
+}
+
+.card-content {
+  text-align: left; /* Align text to the left */
+  display: grid; /* Enable Grid for content */
+}
+
+@media (prefers-color-scheme: light) {
+  :root {
+    --image-background: #e6e8d4; /* Slightly darker image background to contrast white logos */
+    --link-hover-background: #c5b86c; /* Richer gold background on hover */
+    --link-hover-color: #4e4e36; /* Darker text color on hover */
+  }
+
+  .businesscard {
+    background-color: #dce5be; /* Adjusted light green with more depth */
+    color: #757340; /* Darker gold text for better contrast */
+    box-shadow: 0 0 5px rgba(117, 115, 64, 0.5); /* Adjusted gold shadow for better definition */
+  }
+
+  .businesscard-image {
+    background: var(--image-background); /* Uses the darker background variable */
+    /* ... other styles ... */
+  }
+
+  .businesscard-header {
+    color: #9c9a50; /* Less pastel, more pronounced gold */
+    transition: background-color 0.2s ease-in-out, color 0.2s ease-in-out;
+  }
+
+  .businesscard-header:hover,
+  .businesscard-header:focus {
+    background-color: var(--link-hover-background);
+    color: var(--link-hover-color);
+    text-decoration: none; /* Optional: remove if you want underline on hover */
+  }
+
+  .businesscard-link {
+    color: #9c9a50; /* Less pastel, more pronounced gold */
+    transition: background-color 0.2s ease-in-out, color 0.2s ease-in-out;
+  }
+
+  .businesscard-link:hover,
+  .businesscard-link:focus {
+    background-color: var(--link-hover-background);
+    color: var(--link-hover-color);
+    text-decoration: none; /* Optional: remove if you want underline on hover */
+  }
+}
+
+@media (prefers-color-scheme: dark) {
+
+  .businesscard {
+    background-color: #3d5a4c; /* Soft dark green background */
+    color: #c5b358; /* Soft gold text */
+    box-shadow: 0 0 5px rgba(197, 179, 88, 0.5); /* Softer gold shadow */
+  }
+
+  .businesscard-header {
+    color: #c9b037; /* Paler gold links */
+    transition: background-color 0.2s ease-in-out, color 0.2s ease-in-out;
+  }
+
+  .businesscard-head-link {
+    color: #c9b037; /* Paler gold links */
+    transition: background-color 0.2s ease-in-out, color 0.2s ease-in-out;
+  }
+
+  .businesscard-head-link:hover,
+  .businesscard-head-link:focus {
+    background-color: var(--link-hover-background);
+    color: var(--link-hover-color);
+    text-decoration: none; /* Optional: remove if you want underline on hover */
+  }
+
+  .businesscard-link {
+    color: #c9b037; /* Paler gold links */
+    transition: background-color 0.2s ease-in-out, color 0.2s ease-in-out;
+  }
+
+  .businesscard-link:hover,
+  .businesscard-link:focus {
+    background-color: var(--link-hover-background);
+    color: var(--link-hover-color);
+    text-decoration: none; /* Optional: remove if you want underline on hover */
+  }
+}
+
+@media only screen and (max-width: 360px) {
+  .businesscard {
+    grid-template-areas:"header header header"
+    "tags tags tags"
+    "links links links";
+    grid-template-columns: 1fr auto;
+    grid-template-rows: auto auto;
+    text-align: center;
+  }
+
+}
+
+@media only screen and (max-width: 300px) {
+  .businesscard-link {
+    border-radius: .25rem;
+    display: inline-block;
+    font-size: .55rem;
+    font-weight: 700;
+    line-height: 1.5em;
+    margin-bottom: auto;
+    margin-top: auto;
+    padding: 0.25rem;
+  }
+  .businesscard-header {
+
+    font-size: 1em;
+    font-weight: 600;
+    line-height: 1.4em;
+  }
+}
+
+@media only screen and (max-width: 360px) {
+  .imgshow {
+      visibility: hidden;
+  }
+}
+
+@media only screen and (max-width: 460px) {
+  .dnbshow {
+      visibility: hidden;
+  }
+}
+
+@media only screen and (max-width: 410px) {
+  .linkinshow {
+      display: none;
+  }
+}
+
+@media only screen and (max-width: 700px) {
+  .businesscard-image {
+      width: 80%;
+  }
+}
+
+@media only screen and (max-width: 700px) {
+  .businesscard-image {
+      width: 80%;
+  }
+}
+
+@media only screen and (max-width: 500px) {
+  .businesscard {
+    padding: .25rem;
+  }
+
+  .businesscard-image {
+      width: 75%;
+  }
+}
+
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,68 @@
     export let data;
 
     import BusinessCardsList from "$lib/components/businesscards/BusinessCardsList.component.svelte";
+    import defferentiator from '$lib/images/defferentiator-circular-logo-25.png'
 </script>
 
-<h1>Defferent Businesses</h1>
+<body id="root">
+    <div class="Titlelist">
+    <h1>Businesses</h1> <img src={defferentiator} alt={"logo"} class={["logo"].join(' ')} />
+    </div>
+    <BusinessCardsList businesscards={data.Businesses} />
+</body>
 
-<BusinessCardsList businesscards={data.Businesses} />
+<style>
+
+#root {
+  max-width: 850px;
+  margin: auto auto;
+  padding: .5rem;
+  text-align: center;
+}
+
+.Titlelist {
+  display: flex;
+  flex-wrap: flex-start;
+  justify-content: center;
+  margin: 0 auto;
+  padding: 0;
+  list-style: none;
+}
+
+.logo {
+  max-width: 3em;
+  padding: 1em;
+}
+
+
+    @media (prefers-color-scheme: dark) {
+  :root {
+    --image-background: #4f7565; /* Dark mode image background */
+    --link-hover-background: #50655e; /* Darker green background on hover */
+    --link-hover-color: #e0d068; /* Lighter gold color on hover */
+  }
+
+
+  body {
+    background: #2e433a; /* Muted dark green background */
+    color: #c5b358; /* Softer gold text */
+  }
+
+}
+
+@media (prefers-color-scheme: light) {
+  :root {
+    --image-background: #e6e8d4; /* Slightly darker image background to contrast white logos */
+    --link-hover-background: #c5b86c; /* Richer gold background on hover */
+    --link-hover-color: #4e4e36; /* Darker text color on hover */
+  }
+
+  body {
+    background: #d6e8d4; /* Less pastel, more subdued green background */
+    color: #757340; /* Darker gold text for better readability */
+  }
+
+}
+
+
+</style>
