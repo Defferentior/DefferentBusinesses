c1dfa108f421f196c09e85a9dd6b179ad87f8a2f
Temct <150273475+Defferentior@users.noreply.github.com>
2024-10-20 07:32:50 -0700

    Push Google Tag

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<svelte:head>
+    <!-- Google tag (gtag.js) -->
+<!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-XZ9286HV3Z"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-XZ9286HV3Z');
+    </script>
+    <title>Defferentiator</title>
+</svelte:head>
+
+<main>
+
+    <slot></slot>
+</main>
